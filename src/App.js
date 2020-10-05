42d64037d5c42f4370604093c4fe085f1a5d559a
Decston <alissonpatriciodefreitas@gmail.com>
2020-10-05 16:28:16 -0300

    Configurado o tema que vai ser utilizado com Material Ui.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#009688',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
