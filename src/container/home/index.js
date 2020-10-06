3735880a21c940a842ae6ebeec10bcc8bffc6cae
Decston <alissonpatriciodefreitas@gmail.com>
2020-10-06 16:57:41 -0300

    Refatoração da Home Page, dividindo em componentes.

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Divider from '@material-ui/core/Divider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import './Home.css';
+import CustomCard from '../../common/CustomCard';
 
 const Home = (props) => (
     <div className="home-container">
-        <div className="new-list-container">
-            <Card className="card">
-                <CardActionArea className="card-action-area">
-                    <CardContent className="card-content">
-                        <div>
-                            <p className="title">
-                                Adicionar Novas Listas!
-                            </p>
-                        </div>
-                    </CardContent> 
-                </CardActionArea>
-            </Card>
-        </div>
+        <CustomCard containerClass="new-list-container" >
+            <div>
+                <p className="title">Adicionar Novas Listas!</p>
+            </div>
+        </CustomCard>
 
+        <CustomCard 
+            containerClass="list-container" 
+            footer={
+                <div className="list-footer">
+                    <p>06/10/2020</p>
+                    <p>100.00</p>
+                </div>
+            }
+        >
+            <div>
+                <p className="title">Mês</p>
+                <div className="list-card-body">
+                    <div className="list-card-item">
+                        <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
+                        <p>1 item(s) Restantes</p>
+                    </div>
+                    <div className="list-card-item">
+                        <FontAwesomeIcon icon={faCheck} size="sm" />
+                        <p>2 item(s) Comprados</p>
+                    </div>
+                </div>
+            </div>
+        </CustomCard>
+
+        {/*
         <div className="list-container">
             <Card className="card">
                 <CardActionArea className="card-action-area">
@@ -50,7 +63,8 @@ const Home = (props) => (
                     <p>100.00</p>
                 </CardActions>
             </Card>
-        </div>    
+        </div> 
+        */}   
     </div>
 )
 
