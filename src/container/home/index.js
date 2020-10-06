4887615da2ffec5fbe6a5336ea3d8cf106267a8b
Decston <alissonpatriciodefreitas@gmail.com>
2020-10-06 14:05:06 -0300

    Criado o componente e a estilização do botão Adicionar Listas!

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+
+import './Home.css';
 
 const Home = (props) => (
-    <div>
-        <h1>Hello World!</h1>
+    <div className="home-container">
+        <div className="new-list-container">
+            <Card className="card">
+                <CardActionArea className="card-action-area">
+                    <CardContent className="card-content">
+                        <div>
+                            <p className="title">
+                                Adicionar Novas Listas!
+                            </p>
+                        </div>
+                    </CardContent> 
+                </CardActionArea>
+            </Card>
+        </div>
     </div>
 )
 
