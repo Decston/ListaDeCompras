344435061e0c2859f7b8915fe00e80a5ad184144
Decston <alissonpatriciodefreitas@gmail.com>
2020-10-06 15:37:45 -0300

    Criação do icone com o link para a lista do mês

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import './Home.css';
 
@@ -20,6 +24,33 @@ const Home = (props) => (
                 </CardActionArea>
             </Card>
         </div>
+
+        <div className="list-container">
+            <Card className="card">
+                <CardActionArea className="card-action-area">
+                    <CardContent className="card-content">
+                        <div>
+                            <p className="title">Mês</p>
+                            <div className="list-card-body">
+                                <div className="list-card-item">
+                                    <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
+                                    <p>1 item(s) Restantes</p>
+                                </div>
+                                <div className="list-card-item">
+                                    <FontAwesomeIcon icon={faCheck} size="sm" />
+                                    <p>2 item(s) Comprados</p>
+                                </div>
+                            </div>
+                        </div>
+                    </CardContent> 
+                </CardActionArea>
+                <Divider />
+                <CardActions className="card-footer">
+                    <p>06/10/2020</p>
+                    <p>100.00</p>
+                </CardActions>
+            </Card>
+        </div>    
     </div>
 )
 
